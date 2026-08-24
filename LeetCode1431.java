9c50b12d33cd5db068a276d60a807bc321e7497f
Ankit <ankitkumarsinghcoc5631@gmail.com>
2026-08-25 03:11:37 +0530

    Add Java DSA solutions

---
+++ LeetCode1431.java
@@ -0,0 +1,28 @@
+
+import java.util.*;
+
+    public class practise {
+    public static void main(String[] args) {
+        int[] candies = {2, 3, 5, 1, 3};
+        int extraCandies = 3;
+        
+        List<Boolean> result = kidsWithCandies(candies, extraCandies);
+        System.out.println(result);
+    }
+    
+    public static List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+        int maxCandies = 0;
+        for (int candy : candies) {
+            maxCandies = Math.max(maxCandies, candy);
+        }
+
+        List<Boolean> result = new ArrayList<>();
+        for (int i = 0; i < candies.length; i++) {
+            result.add(candies[i] + extraCandies >= maxCandies);
+        }
+
+        return result;
+    }
+    }
+    
+    
