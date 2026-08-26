75cf188eb3f4bfc295c7319baa15498354d21af4
Ankit kumar <ankitkumarsinghcoc5631@gmail.com>
2026-08-26 22:50:39 +0530

    LeetCode-136

---
+++ leetcode136.java
@@ -0,0 +1,46 @@
+
+
+
+//Problem: Given an array where every element appears twice except one, find that single element.
+//Input: [2, 2, 1]     → Output: 1
+//Input: [4, 1, 2, 1, 2] → Output: 4
+
+
+
+
+
+/*import java.util.Arrays;
+public class leetcode136 {
+    public static void main(String[] args) {
+       int[] arrs ={ 4, 1, 2, 1, 2};
+       
+    Arrays.sort(arrs);
+
+    
+  for(int i=0; i<arrs.length-1; i+=2) { 
+    if(arrs[i]!=arrs[i+1]) {
+      System.out.println(arrs[i]);
+      return;
+    }
+  }
+   
+        System.out.println(arrs[arrs.length-1]);
+            }
+        }*/
+    
+    public class leetcode136 {
+      public static void main(String[] args) {
+        int [] arrs = {4,1,2,1,2};
+        int result = 0;
+        for(int num : arrs) {
+          result^=num;
+
+        }
+System.out.println(result);
+      }
+    }
+
+
+
+    
+
