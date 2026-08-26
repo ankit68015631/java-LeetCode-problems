67d3402397431c51c70ee19c3c179f6819bcc44a
Ankit kumar <ankitkumarsinghcoc5631@gmail.com>
2026-08-26 22:48:40 +0530

    LeetCode-169

---
+++ leetcode169.java
@@ -0,0 +1,36 @@
+//Find the element that appears more than n/2 times in an array.
+//Input: [3, 2, 3]                → Output: 3
+//Input: [2, 2, 1, 1, 1, 2, 2]   → Output: 2
+
+
+/*import java.util.Arrays;
+public class leetcode169 {
+    public static void main(String[] args) {
+     int[] nums = {2,2,1,1,1,2,2};
+     Arrays.sort(nums);
+    
+     int result = nums[nums.length/2];
+     System.out.println(result);
+        }
+
+       
+    }*/
+
+
+public class leetcode169 {
+    public static void main(String[] args) {
+     int[] nums = {2,2,1,1,1,2,2};
+     
+     
+    
+    }
+
+        
+        }
+
+       
+    
+
+
+
+
