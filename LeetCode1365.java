0afba560c95b3799c34e68b3718f4175cc359643
Ankit kumar <ankitkumarsinghcoc5631@gmail.com>
2026-08-27 03:00:25 +0530

    LeetCode-1365

---
+++ LeetCode1365.java
@@ -0,0 +1,29 @@
+import java.util.Arrays;
+
+public class LeetCode1365 {
+    public static void main(String[] args) {
+        int[] nums = {8, 1, 2, 2, 3};
+        
+        int[] result = smallerNumbersThanCurrent(nums);
+        System.out.println(Arrays.toString(result));
+    }
+    
+   public static int[] smallerNumbersThanCurrent(int[] nums) {
+    int[] newnums = new int[nums.length];
+
+    for (int i = 0; i < nums.length; i++) {
+        int count = 0;
+
+        for (int j = 0; j < nums.length; j++) {
+            if (nums[i] > nums[j]) {
+                count++;
+            }
+        }
+
+        newnums[i] = count;
+    }
+
+    return newnums;
+  }
+
+}
